bcc58888e51a30393167146c472fe33e0fba2105
Felix2114 <zS23004696@estudiantes.uv.mx>
2025-10-17 01:31:43 -0600

    create nav base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,18 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('titulo', 'ElectroSmart')</title>
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+
     <header>
-        <h1>ElectroSmart</h1>
+       
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="{{ route('inicio') }}">ElectroSmart</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarMenu">
+                    <ul class="navbar-nav ms-auto">
+                         <li class="nav-item"><a class="nav-link" href="{{ route('buscar') }}">Buscar</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('inicio') }}">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('perfil') }}">Perfil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('catalogo') }}">Categorías</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('contacto') }}">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 
-    <main>
+    <main class="container mt-4">
         @yield('contenido')
     </main>
 
-    <footer>
+    <footer class="text-center py-3 bg-light border-top">
         <p>© 2025 ElectroSmart</p>
     </footer>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
