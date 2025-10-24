28c20e60fe185084e9bd955217c9b1ecb0294e95
Aldair0707 <aldairhernandez07124@gmail.com>
2025-10-24 07:17:40 -0600

    add categorias

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,6 +6,7 @@
 
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
